6916d33d7a063cffd63849cfecebd747fa0e5a45
vaibhavvsharma <35368146+vaibhavvsharma@users.noreply.github.com>
2023-11-21 18:52:46 +0530

    trial

---
+++ reactToDo/src/App.jsx
@@ -0,0 +1,57 @@
+import { NewTodoForm } from "./NewToDoForm";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  
+
+  function toggleToDo(id, completed) {
+    setTodos((currentToDos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+    <NewTodoForm />
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        {todos.length === 0 && "No Todos"}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleToDo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
